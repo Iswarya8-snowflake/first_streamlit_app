cccbfc85e56a1e237db64f51f9db61c1b8c49037
Iswarya8-snowflake <148612079+Iswarya8-snowflake@users.noreply.github.com>
2023-10-25 14:42:16 +0530

    Update streamlit_app.py
    Send the Input to Fruityvice as Part of the API Call

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,8 +23,13 @@ streamlit.dataframe(fruits_to_show)
 #New section to display Header for Fruityvice API response
 
 streamlit.header("Fruityvice Fruit Advice!")
+
+#Send the Input to Fruityvice as Part of the API Call
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "Kiwi")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 #streamlit.text(fruityvice_response.json()) #just writes data to screen
 
