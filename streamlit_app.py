e37033833f2a6cd0c56c31bf2bbc5d7ac2f55c66
Iswarya8-snowflake <148612079+Iswarya8-snowflake@users.noreply.github.com>
2023-10-26 10:32:25 +0530

    Update streamlit_app.py
    introducing structure changes to code try except

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,18 +28,20 @@ streamlit.dataframe(fruits_to_show)
 #New section to display Header for Fruityvice API response
 
 streamlit.header("Fruityvice Fruit Advice!")
+try:
+  #Send the Input to Fruityvice as Part of the API Call
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    stremlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    #streamlit.text(fruityvice_response.json()) #just writes data to screen
+    #normalize the json version of response
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
-#Send the Input to Fruityvice as Part of the API Call
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#streamlit.text(fruityvice_response.json()) #just writes data to screen
-
-#normalize the json version of response
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 #Control over code
 streamlit.stop()
