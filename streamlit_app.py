7fbd72764eab938d1fc14be5838ad0e33ddab033
Iswarya8-snowflake <148612079+Iswarya8-snowflake@users.noreply.github.com>
2023-10-26 10:33:58 +0530

    Update streamlit_app.py
    change typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ try:
   #Send the Input to Fruityvice as Part of the API Call
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
-    stremlit.error("Please select a fruit to get information.")
+    streamlit.error("Please select a fruit to get information.")
   else:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     #streamlit.text(fruityvice_response.json()) #just writes data to screen
